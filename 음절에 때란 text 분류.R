d1954afee0c5550a5c88ed85abfa1228ce9a303b
MA-KU <32790261+MAKU315@users.noreply.github.com>
2018-03-24 21:35:01 +0900

    Update and rename text 처리_R.R to 음절에 때란 text 분류.R

--- 음절에 때란 text 분류.R
+++ 음절에 때란 text 분류.R
@@ -1,17 +1,10 @@
-getwd()
-
-?read.table
 doc<-unlist(read.table("the_rainbow_passage_revised.txt",header = FALSE))
-
 
 doc<-as.character(doc)
 doc1<-unlist(strsplit(gsub("s$","",gsub("'","",gsub(" ", "" , gsub("\\,","",gsub("\\." , "",tolower(doc)))))),"-"))
 
-
 barplot(table(doc1))
-
 nchar( gsub( "[^X]", "", gsub( "[aeiouy]+", "X",doc1)))
-
 
 
 # 패키지 사용
@@ -20,13 +13,8 @@ library(qdap)
 syllable_sum(doc1)
 
 
-
-
-
-
-
-
-   gsub("([ae,ee,ue,ou,oo])","i",doc1)
+# 다른 방법
+gsub("([ae,ee,ue,ou,oo])","i",doc1)
 grep(c("a","e","i","o","u"),strsplit(doc1[43],""))
 strsplit(doc1[43],c("a","e","i","o","u"))
 
@@ -41,36 +29,16 @@ txt <- c("The", "licenses", "for", "most", "software", "are",
          "free", "for", "all", "its", "users")
 ( i <- grep("[gu]", txt) ) # indices
      
-     ?gsub
+
 strsplit(doc1," ")
-
-table(doc1)
-
-str(as.data.frame(doc1))
-
-doc1[1]
-
-str(doc1)
-
-View(doc1)
-
-doc[1,1]
 strsplit(doc,",")
 strsplit(doc)
-?gsub
 
 grep("[a-z]", letters)
 gsub("([ab])", "\\1_\\1_", "abc and ABC")
 
-
-?strsplit
-
-
 x <- c(as = "asfef", qu = "qwerty", "yuiop[", "b", "stuff.blah.yech")
 # split x on the letter e
 strsplit(x, "e")
-
-
 a <- "stackoverflow"
-
 b <- gsub("[^aeiouAEIOU]","C",a)
